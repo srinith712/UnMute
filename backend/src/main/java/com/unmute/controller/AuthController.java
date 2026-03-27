af19270d9b3b2ca96d580ca503547a987210331e
SrinithReddy <srinithreddy678@gmail.com>
2026-03-27 21:17:44 +0530

    changes

--- backend/src/main/java/com/unmute/controller/AuthController.java
+++ backend/src/main/java/com/unmute/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "https://unmute-six.vercel.app")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
